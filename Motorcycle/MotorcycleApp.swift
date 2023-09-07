31188562c74e71b7b9cc1f75cb97b21ff3e70e41
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-09-07 22:41:43 +0700

    Initial Commit

---
+++ Motorcycle/MotorcycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorcycleApp.swift
+//  Motorcycle
+//
+//  Created by Azzam Ubaidillah on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MotorcycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
